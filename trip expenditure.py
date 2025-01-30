2f5e0b0d3aea8b354440fbb7f7b28d756ee1c028
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-01-30 17:03:19 +0530

    rr

--- trip expenditure.py
+++ trip expenditure.py
@@ -1,4 +1,4 @@
-def hote_cot(nights):
+def hotel_cost(nights):
     return 140*nights
 def plane_ride_cost(city):
     if "charlotte"==city:
@@ -17,9 +17,9 @@ def rental_car_cost(days):
     else:
         return 40*days
 def trip_cost(city,days,spending_money):
-    return rental_car_cost(days)+hotel_cost(days)+plan_ride_cost(city)+spending_money
+    return rental_car_cost(days)+hotel_cost(days)+plane_ride_cost(city)+spending_money
 print("cost of car rental",rental_car_cost(5))
-print("cost of plane ride"),plan_ride_cost(los angeles)
+print("cost of plane ride"),plane_ride_cost("los angeles")
 print("cost of hotel room",hotel_cost(7))
 print("total cost of trip",trip_cost,"los angeles",7,500)
-print("trip cost("tampa),6,500")
+print("trip cost",("tampa",6,500))
