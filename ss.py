c7c8497c2474330f3c0f1b4c0ef0b385562b66d7
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-02-13 17:05:39 +0530

    cc

---
+++ ss.py
@@ -0,0 +1,9 @@
+# Define two sets
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+# Calculate the symmetric difference
+symmetric_difference = set_a.symmetric_difference(set_b)
+
+# Print the result
+print("Symmetric Difference:", symmetric_difference)
