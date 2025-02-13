94d044ccca80013b5b70209fc22e41f8e0f41d4e
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-02-13 16:47:09 +0530

    dd

---
+++ dd.py
@@ -0,0 +1,14 @@
+my_set={1,2,3}
+print(my_set)
+my_set=[1.0,"hello",(1,2,3)]
+print(my_set)
+my_set={1,2,3,4,3,2,1}
+print(my_set)
+my_set=set([1,2,3,2])
+print(my_set)
+num_set=([0,1,3,4,5])
+print("original set:")
+print(num_set)
+num_set.pop()
+print("set:")
+print(num_set)
