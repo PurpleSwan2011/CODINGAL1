a69a11ca0dae3ab866f83e78fcae1e0a8910b4ba
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-02-24 12:00:50 +0530

    f

---
+++ cv.py
@@ -0,0 +1,7 @@
+class Vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed 
+        self.max.milage=mileage
+modelX=Vehicle(240,18)
+print("m:",modelX.max_speed)
+print("m:",modelX.mileage)
