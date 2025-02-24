0036db1f34fefcf63c0ed35d1863dcac63d7b6b5
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-02-24 12:03:00 +0530

    d

--- cv.py
+++ cv.py
@@ -1,7 +1,7 @@
 class Vehicle:
     def __init__(self,max_speed,mileage):
         self.max_speed=max_speed 
-        self.max.milage=mileage
+        self.mileage=mileage
 modelX=Vehicle(240,18)
 print("m:",modelX.max_speed)
 print("m:",modelX.mileage)
