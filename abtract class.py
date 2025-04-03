b9c138db0765f4b46dd960436cc214324ace002e
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-04-03 16:35:22 +0530

    w

---
+++ abtract class.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+class Absclass(ABC):
+    def print(self,x):
+        print("Passed Value:",x)
+    @abstractmethod
+    def task(self):
+        print("v r inside Absclass task")
+class test_class(Absclass):
+    def task(self):
+        print("v r inside text_class task")
+test_obj=test_class()
+test_obj.task()
+test_obj.print(100)
