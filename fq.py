777ad1800f335f18d5b63ab23aafc42d2fc8cfaa
PurpleSwan2011 <saima.replit2011@gmail.com>
2025-04-07 20:44:43 +0530

    g

---
+++ fq.py
@@ -0,0 +1,20 @@
+import random
+class FruitQuiz:
+        def __init__(self):
+                self.fruits={'apple':'red','orange':'orange','watermelon':'green','banana':'yellow'}
+        def quiz(self):
+                while(True):
+                       fruit,color=random.choice(list(self.fruits.items()))
+                       print("what is the color of {}".format(fruit))
+                       user_answer=input()
+if(user_answer.lower()==color):
+    
+    print("c ans")
+else:
+    print("wr ans")
+option=int(input("enter"))
+if(option):
+       break
+print("welcs")
+fq=FruitQuiz()
+fq.quiz()
